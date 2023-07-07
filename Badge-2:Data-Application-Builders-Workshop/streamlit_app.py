d22c1dde7fbaa9cc9df679ff21ab57408f597593
luissedas <luis.sedas1@gmail.com>
2023-07-07 17:10:56 -0500

    add default multiselect

--- Badge-2:Data-Application-Builders-Workshop/streamlit_app.py
+++ Badge-2:Data-Application-Builders-Workshop/streamlit_app.py
@@ -15,7 +15,10 @@ streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_select = streamlit.multiselect("Pick some fruits:",
+                                      list(my_fruit_list.index),
+                                      ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_select]
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
