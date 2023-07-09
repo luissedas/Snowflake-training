caba43f38282f7b82ccea4a293147aa7cc54f4dc
luissedas <luis.sedas1@gmail.com>
2023-07-09 17:04:02 -0500

    added snowflaje connection and print

--- Badge-2:Data-Application-Builders-Workshop/streamlit_app.py
+++ Badge-2:Data-Application-Builders-Workshop/streamlit_app.py
@@ -36,3 +36,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalize)
+
+# Add Snowflake connection
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
